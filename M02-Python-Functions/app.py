1eee394077a2ccc4b1beeb8978cc5c9dd4d99ee6
Anirban Datta <anirban.datta.ds@gmail.com>
2022-04-14 22:50:29 +0530

    Learned about *args and **kwargs.

--- M02-Python-Functions/app.py
+++ M02-Python-Functions/app.py
@@ -1,9 +1,18 @@
-def my_function(a, b):
-    return a + b
+# def my_function(a, b, *arg):
+#     return a + b
 
-a = float(input("Enter a number: "))
-b = float(input("Enter another number: "))
+# a = float(input("Enter a number: "))
+# b = float(input("Enter another number: "))
 
-result = "{:.2f}".format(my_function(a, b))
+# result = "{:.2f}".format(my_function(a, b))
 
-print(f'Their sum is {result}.')
+# print(f'Their sum is {result}.')
+
+def adder(*args):
+    sum = 0
+    for num in args:
+        sum += num
+
+    return sum
+
+print(f'Sum is: {adder(1,2,3,4,5)}.')
