d6bef331e13b189a881ecfbf3b3e3cc799d86572
Anirban Datta <anirban.datta.ds@gmail.com>
2022-04-26 19:46:28 +0530

    Py Classes.

---
+++ M01 - Python - Functions/app.py
@@ -0,0 +1,13 @@
+def display():
+    '''
+    Prints a greeting.
+    '''
+
+    print('Hello!')
+
+if __name__ == '__main__':
+    display()
+    print('This code is run as a program.')
+
+else:
+    print('This code is run as a module.')
